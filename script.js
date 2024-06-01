5046896a134143d23245c4023fe27d5ad1b62fcb
imoncrack27 <mingodrexell321@gmail.com>
2024-06-01 23:55:36 +0800

    inputted localStorage for saving data everytime it refreshes

--- script.js
+++ script.js
@@ -8,5 +8,26 @@ function addTask() {
     let li = document.createElement("li");
     li.innerHTML = inputBox.value;
     listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00D7";
+    li.appendChild(span);
   }
+  inputBox.value = "";
+  saveData();
+}
+
+listContainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+    }
+  },
+  false
+);
+
+function saveData() {
+  localStorage.setItem("data", listContainer.innerHTML);
 }
